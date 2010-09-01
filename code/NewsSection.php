bbe56915637f1d2088c219d15521496e093b6c00
admin <admin@5766e509-19ac-44c2-bbb0-941299f47fb9>
2010-09-01 07:58:48 +0000

    added OtherNews control
    git-svn-id: svn://10.0.1.208:4000/silverstripe/modules/mouseketeers/news/trunk@226 5766e509-19ac-44c2-bbb0-941299f47fb9

--- code/NewsSection.php
+++ code/NewsSection.php
@@ -1,15 +1,20 @@
 <?php
-class NewsSection extends Section {
+class NewsSection extends Page {
 	static $db = array();
 	static $has_one = array();
 	//static $allowed_children = array("NewsPage");
  	static $default_child = "NewsPage";
 }
-class NewsSection_Controller extends Section_Controller {
-	public function News($num='',$parent='') {
+class NewsSection_Controller extends Page_Controller {
+	public function News($num=10,$parent='') {
 		$where = '(FromDate IS NULL OR FromDate <= NOW()) AND (ToDate IS NULL OR ToDate >= NOW())';
 		if ($parent) $where .= ' AND ParentID = '.$parent;
- 		return DataObject::get('NewsPage', $where, 'FromDate DESC', '', $num); 
+ 		return DataObject::get('NewsPage', $where, 'FromDate DESC', '', $num);
+	}
+	public function OtherNews($num=10,$parent='') {
+		$where = '`NewsPage`.`ID` <> '.$this->ID .' AND (FromDate IS NULL OR FromDate <= NOW()) AND (ToDate IS NULL OR ToDate >= NOW())';
+		if ($parent) $where .= ' AND ParentID = '.$parent;
+ 		return DataObject::get('NewsPage', $where, 'FromDate DESC', '', $num);
 	}
 }
 ?>
