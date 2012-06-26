49da26f1c853a951d9f70deff2b32a25ad27e71d
Henrik Olsen <henrik@mouseketeers.dk>
2012-06-26 10:30:40 +0200

    Added select for number of news to list + functinality for showing news from all subsections

--- code/NewsSection.php
+++ code/NewsSection.php
@@ -3,55 +3,97 @@ class NewsSection extends Page {
 	public static $db = array(
 		'Pagination' => 'Boolean',
 		'NumPages' => 'Int',
-		'ExcludeOutdatedNews' => 'Boolean'
+		'ExcludeOutdatedNews' => 'Boolean',
+		'IncludeNewsInSubsections' => 'Boolean'
 	);
 	static $defaults = array(
-		'SortOrder' => 'FromDate',
-		'ExcludeOutdatedNews' => '0'
+		'ExcludeOutdatedNews' => '0',
+		'NumPages' => 10
 	);
 	static $has_one = array();
  	static $default_child = "NewsPage";
  	static $icon  = 'news/images/news_section';
- 	
+ 	static $num_pages_options = array(
+		0 => 'All',
+		5 => '5',
+		10 => '10',
+		15 => '15',
+		20 => '20',
+		50 => '50',
+		100 => '100'
+	);
 	function getCMSFields() {
 		$fields = parent::getCMSFields();
 		$fields->addFieldToTab(
 			$tab = 'Root.Content.Main',
-			new NumericField(
+			new DropdownField(
 				$db_name = 'NumPages',
-				$cms_label = _t('NewsSection.MAX_PAGES_TO_LIST','Max pages to list (set to 0 to list all pages)')
-			),
+				$cms_label = _t('NewsSection.MAX_PAGES_TO_LIST','Max news stories to list per page'),self::$num_pages_options),
 			$place_before = 'Content'
 		);
 		$fields->addFieldToTab(
 			$tab = 'Root.Content.Main',
 			new CheckboxField(
 				$db_name = 'ExcludeOutdatedNews',
-				$cms_label = _t('NewsSection.EXCLUDE_OUTDATED','Exclude outdated news from the list')
+				$cms_label = _t('NewsSection.EXCLUDE_OUTDATED','Exclude outdated news')
 			),
 			$place_before = 'Content'
 		);
+		$fields->addFieldToTab(
+			$tab = 'Root.Content.Main',
+			new CheckboxField(
+				$db_name = 'IncludeNewsInSubsections',
+				$cms_label = _t('NewsSection.INCLUDE_SUBSECTIONS','Include news in subsections')
+			),
+			$place_before = 'Content'
+		);
+
 		return $fields;
 	}
 }
 class NewsSection_Controller extends Page_Controller {
-	//gets current news other than the 
-	public function OtherNews($limit='') {
-		$filter = '`NewsPage`.`ID` <> '.$this->ID .' AND (FromDate IS NULL OR FromDate <= NOW()) AND (ToDate IS NULL OR ToDate >= NOW())'; 
-		//if ($parent) $where .= ' AND ParentID = '.$parent;
- 		return DataObject::get('NewsPage', $filter, 'FromDate DESC', '', $limit);
-	}
 	public function ContentList() {
-		$limit = '';
 		if($this->NumPages) {
 			if(!isset($_GET['start']) || !is_numeric($_GET['start']) || (int)$_GET['start'] < 1) $_GET['start'] = 0;
 			$limit_start = (int)$_GET['start'];
 			$limit = $limit_start.','.$this->NumPages;
 		}
-		$filter = 'ParentID = '. $this->ID;
+		else {
+			$limit = '';
+		}
+		if($this->IncludeNewsInSubsections) {
+			$decendent_ids = $this->getDescendantIDList();
+			$filter = '"NewsPage"."ID" IN ('.implode(',',$decendent_ids).') ';
+		}
+		else {
+			$filter = 'ParentID = '. $this->ID;	
+		}
 		if ($this->ExcludeOutdatedNews) $filter .= ' AND (FromDate IS NULL OR FromDate <= NOW()) AND (ToDate IS NULL OR ToDate >= NOW())';
 		$data = DataObject::get('NewsPage', $filter, 'FromDate DESC','',$limit);
 		return $data;
 	}
+	function getAllChildren($objectType = null, $parentObject = null, $dataSet = null) {
+      if ($parentObject == null) {
+         return null;
+      }
+      if ($dataSet == null) {
+         $dataSet = new DataObjectSet();
+      }
+      $children = $parentObject->AllChildren();
+      if (!empty($children)) {
+         $dataSet->merge($children);
+         foreach ($children as $child) {
+               $dataSet = $this->getAllChildren($objectType, $child, $dataSet);
+         }
+      }
+      if ($objectType) {
+         foreach ($dataSet as $object) {
+            if ($object->class != $objectType) {
+               $dataSet->remove($object);
+            }
+         }
+      }
+      return $dataSet;
+   }
 }
 ?>
