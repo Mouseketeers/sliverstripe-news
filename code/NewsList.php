7d3e9946c250faa0e6696c1d202e8b7493798a5e
Henrik Olsen <henrik@mouseketeers.dk>
2013-12-13 12:22:33 +0100

    Added NewsList-feature for listing news on other pages (a la widget). Includes CMS administration under site config

---
+++ code/NewsList.php
@@ -0,0 +1,45 @@
+<?php
+class NewsList extends DataObjectDecorator {
+	function extraStatics() {
+		return array(
+			'db' => array(
+				'NewsListHeadline' => 'Varchar',
+				'NewsListLength' => 'Int',
+				'NewsListNoNewsMessage' => 'Varchar'
+			),
+			'many_many' => array(
+				'NewsSections' => 'NewsSection'
+			),
+			'defaults' => array(
+				'NewsListHeadline' => 'Latest news',
+				'NewsListLength' => 5,
+				'NewsListNoNewsMessage' => 'There are currently no news'
+			)
+		);
+	}
+	function updateCMSFields(&$fields){
+		$newSections = DataObject::get('NewsSection');
+		if($newSections) {
+			$fields->addFieldsToTab( 'Root.Content.News', array(
+				new TextField('NewsListHeadline', 'Headline (e.g. Latest news)'),
+				new TextField('NewsListLength', 'Number of news to list'),
+				new TextField('NewsListNoNewsMessage', 'Message to show when there are no news'),
+				new CheckboxSetfield(
+					'NewsSections',
+					'Exclude the following news sections',
+					$newSections->toDropdownMap('ID','Title')
+				)
+			));
+			return $fields;
+		}
+	}
+	public function News() {
+		$where = 'FromDate IS NULL OR FromDate <= NOW()';
+		$newsSections = $this->owner->NewsSections();
+		if($newsSections->Count()) {
+			$where .= ' AND ParentID NOT IN ('.implode(',',$newsSections->column('ID')).')';
+		}
+		$data = DataObject::get('NewsPage', $where, 'FromDate DESC', '', $this->owner->NewsListLength);
+ 		return $data;
+	}
+}
---
+++ templates/Includes/NewsList.ss
@@ -0,0 +1,18 @@
+<div id="news-widget">
+	<h2>$NewsListHeadline</h2>
+	<% if News %>
+	<div class="content-list">
+		<% control News %>
+		<div class="content-item">
+			<a href="$Link" class="link-block">
+				<h3>$Title</h3>
+				<p class="date"><strong>NEWS</strong> $FromDate.Long</p>
+				<p><% if Abstract %>$Abstract<% else %>$Content.Summary(16)<% end_if %></p>
+			</a>
+		</div>
+		<% end_control %>
+	</div>
+	<% else %>
+	<p>$NewsListNoNewsMessage</p>
+	<% end_if %>
+</div>
